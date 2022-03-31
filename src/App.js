daa73fabdb1c583b08a1d420e5de7d9facd77268
rambey-coder <olayiwolaagboolaakanji@gmail.com>
2022-03-31 14:57:35 +0100

    preloading effect

--- src/App.js
+++ src/App.js
@@ -1,12 +1,47 @@
 import React from "react";
+import { useState, useEffect } from "react";
 import Navbar from './Components/Navbar'
 import Intro from "./Components/Intro";
+import HashLoader from "react-spinners/HashLoader";
 
 function App() {
+  const [loading, setLoading] = useState(false);
+
+    useEffect(() => {
+        setLoading(true)
+        setTimeout(() => {
+            setLoading(false)
+        }, 9000)
+    },[])
+
+    const override = `
+      background: none;
+    `
+
   return (
     <div className="App">
-      <Navbar />
-      <Intro />
+      {
+        loading ? 
+
+        <div className="loaders">
+        <HashLoader
+        className= 'loader'
+        size={50}
+        background='transparent'
+        css={override}
+        color={"#B01CCC"}
+        loading={loading}
+        />
+        </div>
+
+        :
+
+        <>
+        <Navbar />
+        <Intro />
+        </>
+      }
+
     </div>
   );
 }
