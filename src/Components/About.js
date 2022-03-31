f90c603ce5da716c2ce203ff47cb1f5e1400184b
rambey-coder <olayiwolaagboolaakanji@gmail.com>
2022-03-31 10:25:33 +0100

    Started about section

--- src/Components/About.js
+++ src/Components/About.js
@@ -1,9 +1,19 @@
 import React from 'react'
+import './About.css'
 
 const About = () => {
   return (
     <div className="abt-container" id="About">
-        
+        <div className="sub-container">
+          <div className="block-flex">
+            <p>Hi, my name is</p>
+            <h2>Bello Ramadan.</h2>
+            <h1>I build things for the web.</h1>
+            <h3>FRONT-END WEB DEVELOPER.</h3>
+            <p>Innovative Entry-level Front End Developer with good years of experience in maintaining and building web pages. I like to craft solid and scalable frontend products with great user experiences. Highly skilled at progressive enhancement, design systems & UI Engineering.</p>
+            <button>Hire Me</button>
+          </div>
+        </div>
     </div>
   )
 }
