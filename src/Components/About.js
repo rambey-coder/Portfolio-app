7efd0af6e3c10dd523f0f4a802ce089513643a6e
unknown <olayiwolaagboolaakanji@gmail.com>
2022-04-22 15:24:16 +0100

    grammar edited

--- src/Components/About.js
+++ src/Components/About.js
@@ -11,11 +11,11 @@ const About = () => {
          </div> 
         <div className="general-card" >
         <div className="card1" >
-        <p> Hello!My name is Ramadan, and I enjoy creating things that live on the internet.My interest in web development started a long time ago when I decided to
+        <p> Hello! My name is Ramadan, and I enjoy creating things that live on the internet. My interest in web development started a long time ago when I decided to
         try editing custom wordpress themes, which made me develop huge interest in web development. </p>
 
         <p>
-        Fast - forward to today, and I’ ve had the privilege of working on many projects using differnt tools such as HTML, CSS, JAVASCRIPT, REACT JS.My main focus these days is building accessible, inclusive products and digital experiences at Upstatement
+        Fast - forward to today, and I’ ve had the privilege of working on many projects using different tools such as HTML, CSS, JAVASCRIPT, REACT JS. My main focus these days is building accessible, inclusive products and digital experiences at Upstatement
         for a variety of clients. </p>
 
         <p>Here are technologies I’ ve been working with recently: </p>
