954a0cb58ccb2134e1733de6aa33aed9aa71406e
Rambey <71680392+rambey-coder@users.noreply.github.com>
2023-11-12 16:03:35 +0100

    Update Intro.js

--- src/Components/Intro.js
+++ src/Components/Intro.js
@@ -30,9 +30,9 @@ const Intro = () => {
           <h1>I build things for the web.</h1>
           <h3>FRONT-END WEB DEVELOPER.</h3>
           <p>Innovative Front End Developer with good experience in maintaining and building web pages. I like to craft solid and scalable frontend products with great user experiences. Highly skilled at progressive enhancement, design systems & UI Engineering.</p>
-          <a className='button' href='https://drive.google.com/file/d/10jUiT1WkMH42VTbf6vYK3cWZBmHaywPe/view?usp=sharing'>View Resume</a>
+          <a className='button' href='https://drive.google.com/file/d/1bxDcJ3A7C2EL7TkQA7ZdwDx2XlQfavPQ/view?usp=drive_link'>View Resume</a>
 
-          <a className='button' href='https://drive.google.com/file/d/14gt9Dq1w0sprestwQ0Ad9WzT4jopR_V4/view?usp=sharing' target='_blank'>View Cover Letter</a>
+          <a className='button' href='https://drive.google.com/file/d/1UDhNPP8lhK85qUXWLdmwvnoEoL5hZcfy/view?usp=sharing' target='_blank'>View Cover Letter</a>
         </div>
       </div>
     </div>
